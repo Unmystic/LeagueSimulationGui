1a3fb3179225d8f314b102c2dfc776e04573b28c
Hedoruto <Unmystic7@gmail.com>
2023-02-03 20:15:29 +0300

    Basic Test XX (New Windows)

--- app.py
+++ app.py
@@ -22,20 +22,36 @@ class AnotherWindow(QWidget):
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.w = None
+        self.w1 = AnotherWindow()
+        self.w2 = AnotherWindow()
+
+        l = QVBoxLayout()
         self.setWindowTitle("My App")
         self.setMinimumSize(QSize(640, 480))
-        self.button = QPushButton("Push for Window")
-        self.button.clicked.connect(self.show_new_window)
-        self.setCentralWidget(self.button)
 
-    def show_new_window(self, checked):
-        if self.w is None:
-            self.w = AnotherWindow()
-            self.w.show()
+        button1 = QPushButton("Push for Window1")
+        button1.clicked.connect(
+            lambda checked: self.toggle_window(self.w1)
+        )
+        l.addWidget(button1)
+
+        button2 = QPushButton("Push for Window1")
+        button2.clicked.connect(
+            lambda checked: self.toggle_window(self.w2)
+        )
+        l.addWidget(button2)
+
+        w = QWidget()
+        w.setLayout(l)
+        self.setCentralWidget(w)
+
+    def toggle_window(self, window):
+        if window.isVisible():
+            window.hide()
         else:
-            self.w.close()
-            self.w = None
+            window.show()
+
+
 
 
 app = QApplication(sys.argv)
