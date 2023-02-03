952a76e9a49bb32898f6c98e71f7d04e44cdb146
Hedoruto <Unmystic7@gmail.com>
2023-02-03 19:37:21 +0300

    Basic Test XIX (Temp Windows)

--- app.py
+++ app.py
@@ -1,34 +1,42 @@
+import random
 import sys
 
-from PyQt6.QtWidgets import QApplication, QDialog, QMainWindow, QMessageBox, QPushButton
+from PyQt6.QtCore import QSize
+from PyQt6.QtWidgets import QApplication, QDialog, QMainWindow, QMessageBox, QPushButton, QWidget, QVBoxLayout, QLabel
+
+
+class AnotherWindow(QWidget):
+    """
+    This window is a Qwidget. if it has no parent,
+    it appear as floating window.
+    """
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        self.setMinimumSize(QSize(320, 240))
+        self.label = QLabel(f"Another window {random.randint(0,100)}")
+        layout.addWidget(self.label)
+        self.setLayout(layout)
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
+        self.w = None
         self.setWindowTitle("My App")
+        self.setMinimumSize(QSize(640, 480))
+        self.button = QPushButton("Push for Window")
+        self.button.clicked.connect(self.show_new_window)
+        self.setCentralWidget(self.button)
 
-        button = QPushButton("Press me for a dialog!")
-        button.clicked.connect(self.button_clicked)
-        self.setCentralWidget(button)
-
-    def button_clicked(self, s):
-
-        button = QMessageBox.critical(
-            self,
-            "Question dialog",
-            "The longer message",
-            buttons=QMessageBox.StandardButton.Discard | QMessageBox.StandardButton.NoToAll | QMessageBox.StandardButton.Ignore,
-            defaultButton=QMessageBox.StandardButton.Discard,
-        )
-
-        if button == QMessageBox.StandardButton.Discard:
-            print("Discard!")
-        elif button == QMessageBox.StandardButton.NoToAll:
-            print("No to all!!")
+    def show_new_window(self, checked):
+        if self.w is None:
+            self.w = AnotherWindow()
+            self.w.show()
         else:
-            print("Ignore!")
+            self.w.close()
+            self.w = None
+
 
 app = QApplication(sys.argv)
 
