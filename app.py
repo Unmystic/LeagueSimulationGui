5cdb7e0597637d20882f96a0881e4380fd0bdc2a
Hedoruto <Unmystic7@gmail.com>
2023-02-03 16:38:57 +0300

    Basic Test XVII (Dialog Popup)

--- app.py
+++ app.py
@@ -1,65 +1,49 @@
 import sys
-from PyQt6.QtWidgets import (
-    QMainWindow, QApplication,
-    QLabel, QToolBar, QStatusBar, QCheckBox
-)
-from PyQt6.QtGui import QAction, QIcon, QKeySequence
-from PyQt6.QtCore import Qt, QSize
+
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QDialog, QDialogButtonBox, QVBoxLayout, QLabel
 
 
 class MainWindow(QMainWindow):
-
     def __init__(self):
-        super(MainWindow, self).__init__()
+        super().__init__()
 
-        self.setWindowTitle("My Awesome App")
+        self.setWindowTitle("My App")
 
-        label = QLabel("Hello!")
-        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        button = QPushButton("Press me for a dialog!")
+        button.clicked.connect(self.button_clicked)
+        self.setCentralWidget(button)
 
-        self.setCentralWidget(label)
-
-        toolbar = QToolBar("My main toolbar")
-        toolbar.setIconSize(QSize(16, 16))
-        self.addToolBar(toolbar)
-
-        button_action = QAction(QIcon("ui-button.png"), "Your button", self)
-        button_action.setStatusTip("This is your button")
-        button_action.triggered.connect(self.onMyToolBarButtonClick)
-        button_action.setCheckable(True)
-        # You can enter keyboard shortcuts using either key names(Ctrl+p)
-        # Qt.namespace identifiers(Qt.CTRL + Qt.Key_P)
-        # or system agnostic identifiers( QkeySequence.StandartKey.Print)
-        button_action.setShortcut(QKeySequence("Ctrl+p"))
-        toolbar.addAction(button_action)
-
-        toolbar.addSeparator()
-
-        button_action2 = QAction(QIcon("bug.png"), "Your &button2", self)
-        button_action2.setStatusTip("This is your button2")
-        button_action2.triggered.connect(self.onMyToolBarButtonClick)
-        button_action2.setCheckable(True)
-        toolbar.addAction(button_action2)
-
-        toolbar.addWidget(QLabel("Hello"))
-        toolbar.addWidget(QCheckBox())
-
-        self.setStatusBar(QStatusBar(self))
-
-        menu = self.menuBar()
-
-        file_menu = menu.addMenu("&File")
-        file_menu.addAction(button_action)
-        file_menu.addSeparator()
-        file_submenu = file_menu.addMenu("Submenu")
-        file_submenu.addAction(button_action2)
-
-
-    def onMyToolBarButtonClick(self, s):
+    def button_clicked(self, s):
         print("click", s)
+
+        dlg = CustomDialog(self)
+        if dlg.exec():
+            print("Success!!")
+        else:
+            print("Denied!")
+
+class CustomDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.setWindowTitle("Hello!!!")
+
+        QBtn = QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel
+
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        self.layout = QVBoxLayout()
+        message = QLabel("What should we do?")
+        self.layout.addWidget(message)
+        self.layout.addWidget(self.buttonBox)
+        self.setLayout(self.layout)
 
 
 app = QApplication(sys.argv)
-w = MainWindow()
-w.show()
+
+window = MainWindow()
+window.show()
+
 app.exec()
