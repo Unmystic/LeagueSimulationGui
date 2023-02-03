bf38bb4a81057d06f73a61a01b858e716451855c
Hedoruto <Unmystic7@gmail.com>
2023-02-03 14:08:37 +0300

    Basic Test XV-II (Toolbar)

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import sys
 from PyQt6.QtWidgets import (
     QMainWindow, QApplication,
-    QLabel, QToolBar, QStatusBar
+    QLabel, QToolBar, QStatusBar, QCheckBox
 )
 from PyQt6.QtGui import QAction, QIcon
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, QSize
+
 
 class MainWindow(QMainWindow):
 
@@ -19,7 +20,27 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(label)
 
         toolbar = QToolBar("My main toolbar")
+        toolbar.setIconSize(QSize(16, 16))
         self.addToolBar(toolbar)
+
+        button_action = QAction(QIcon("ui-button.png"), "Your button", self)
+        button_action.setStatusTip("This is your button")
+        button_action.triggered.connect(self.onMyToolBarButtonClick)
+        button_action.setCheckable(True)
+        toolbar.addAction(button_action)
+
+        toolbar.addSeparator()
+
+        button_action2 = QAction(QIcon("bug.png"), "Your &button2", self)
+        button_action2.setStatusTip("This is your button2")
+        button_action2.triggered.connect(self.onMyToolBarButtonClick)
+        button_action2.setCheckable(True)
+        toolbar.addAction(button_action2)
+
+        toolbar.addWidget(QLabel("Hello"))
+        toolbar.addWidget(QCheckBox())
+
+        self.setStatusBar(QStatusBar(self))
 
     def onMyToolBarButtonClick(self, s):
         print("click", s)
