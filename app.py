bb92c0539c828af29593fd8d2b7165c5610ff242
Hedoruto <Unmystic7@gmail.com>
2023-02-04 12:34:03 +0300

    Basic Test XXI (Qt designer - dialog form)

--- app.py
+++ app.py
@@ -1,62 +1,17 @@
-import random
 import sys
+from PyQt6 import QtWidgets, uic
 
-from PyQt6.QtCore import QSize
-from PyQt6.QtWidgets import QApplication, QDialog, QMainWindow, QMessageBox, QPushButton, QWidget, QVBoxLayout, QLabel
-
-
-class AnotherWindow(QWidget):
-    """
-    This window is a Qwidget. if it has no parent,
-    it appear as floating window.
-    """
-    def __init__(self):
-        super().__init__()
-        layout = QVBoxLayout()
-        self.setMinimumSize(QSize(320, 240))
-        self.label = QLabel(f"Another window {random.randint(0,100)}")
-        layout.addWidget(self.label)
-        self.setLayout(layout)
+from dialog import Ui_Dialog
 
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.w1 = AnotherWindow()
-        self.w2 = AnotherWindow()
-
-        l = QVBoxLayout()
-        self.setWindowTitle("My App")
-        self.setMinimumSize(QSize(640, 480))
-
-        button1 = QPushButton("Push for Window1")
-        button1.clicked.connect(
-            lambda checked: self.toggle_window(self.w1)
-        )
-        l.addWidget(button1)
-
-        button2 = QPushButton("Push for Window1")
-        button2.clicked.connect(
-            lambda checked: self.toggle_window(self.w2)
-        )
-        l.addWidget(button2)
-
-        w = QWidget()
-        w.setLayout(l)
-        self.setCentralWidget(w)
-
-    def toggle_window(self, window):
-        if window.isVisible():
-            window.hide()
-        else:
-            window.show()
+class MainWindow(QtWidgets.QDialog, Ui_Dialog):
+    def __init__(self, *args, obj=None, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
 
 
-
-
-app = QApplication(sys.argv)
+app = QtWidgets.QApplication(sys.argv)
 
 window = MainWindow()
 window.show()
-
 app.exec()
