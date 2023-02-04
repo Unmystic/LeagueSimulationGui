73c49e7e5b5e5c980cd7aaa315c476f5fd3007a7
Hedoruto <Unmystic7@gmail.com>
2023-02-04 19:25:14 +0300

    Basic Test XXI - 2 (Python app: main and dialog form)

--- app.py
+++ app.py
@@ -1,17 +1,36 @@
 import sys
-from PyQt6 import QtWidgets, uic
+from PyQt6.QtWidgets import QApplication, QDialog, QMainWindow, QPushButton
 
 from dialog import Ui_Dialog
 
 
-class MainWindow(QtWidgets.QDialog, Ui_Dialog):
-    def __init__(self, *args, obj=None, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
-        self.setupUi(self)
+class MainWindow(QMainWindow):
+    """ Main window"""
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        # Use Push button to activate dialog screen
+        self.centralWidget = QPushButton("Employee")
+        self.centralWidget.clicked.connect(self.onEmployeeBtnClicked)
+        self.setCentralWidget(self.centralWidget)
+
+    def onEmployeeBtnClicked(self):
+        """Launches employee dialog"""
+        dlg = EmployeeDlg(self)
+        dlg.exec()
 
 
-app = QtWidgets.QApplication(sys.argv)
+class EmployeeDlg(QDialog):
+    """Employee dialog"""
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        # Create an instance of GUI
+        self.ui = Ui_Dialog()
+        # Run the .setupUI() method to show the Gui
+        self.ui.setupUi(self)
 
-window = MainWindow()
-window.show()
-app.exec()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
