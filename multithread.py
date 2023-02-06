39d2849d0591a14a8e3c3284afbeb9b5f13e9c4c
Hedoruto <Unmystic7@gmail.com>
2023-02-06 13:38:55 +0300

    Basic Test XXII - 3 (Multithread with signals)

--- multithread.py
+++ multithread.py
@@ -1,8 +1,36 @@
+"""
+Adding multithread capabilities to simple pyQt applications
+"""
+
 from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import *
 
 import time
+import traceback,sys
+
+class WorkerSignals(QObject):
+    """
+    Defines the signals available from a running worker thread
+    Supported signals are
+
+    finished
+        No data
+
+    error
+        tuple(exctype, value, traceback.format_exc())
+
+    result
+        object data returned from processing
+
+    progress
+        int indicating % progress
+
+    """
+    finished = pyqtSignal()
+    error = pyqtSignal(tuple)
+    result = pyqtSignal(object)
+    progress = pyqtSignal(int)
 
 class Worker(QRunnable):
     """
@@ -21,6 +49,10 @@ class Worker(QRunnable):
         self.fn = fn
         self.args = args
         self.kwargs = kwargs
+        self.signals = WorkerSignals()
+
+        #Add the callback to aour kwargs
+        self.kwargs["progress_callback"] = self.signals.progress
 
     @pyqtSlot()
     def run(self):
@@ -28,10 +60,17 @@ class Worker(QRunnable):
         your code goes in this function
         :return:
         """
-        print("Thread start")
-        self.fn(*self.args, **self.kwargs)
-        time.sleep(3)
-        print("Thread complete")
+        try:
+            result = self.fn(*self.args, **self.kwargs)
+        except:
+            traceback.print_exc()
+            exctype, value = sys.exc_info()[:2]
+            self.signals.error.emit((exctype, value, traceback.format_exc()))
+        else:
+            self.signals.result.emit(result)
+        finally:
+            self.signals.finished.emit()
+
 
 
 
@@ -64,11 +103,27 @@ class MainWindow(QMainWindow):
         self.timer.timeout.connect(self.recurring_timer)
         self.timer.start()
 
-    def execute_this_fn(self, name):
-        print(f"Hello, {name}!")
+    def progress_fn(self, n):
+        print("%d%% done" % n)
+
+    def execute_this_fn(self, progress_callback):
+        for n in range(0,5):
+            time.sleep(1)
+            progress_callback.emit((n * 100) // 4)
+        return "Done."
+
+    def print_output(self, s):
+        print(s)
+
+    def thread_complete(self):
+        print("THREAD COMPLETE!")
 
     def oh_no(self):
-        worker = Worker(self.execute_this_fn, name="Dio")
+        worker = Worker(self.execute_this_fn)
+        worker.signals.result.connect(self.print_output)
+        worker.signals.finished.connect(self.thread_complete)
+        worker.signals.progress.connect(self.progress_fn)
+
         self.threadpool.start(worker)
 
     def recurring_timer(self):
